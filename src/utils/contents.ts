d3eabae14ff1c5b19e2532486595445746ca52e7
Daniele Benedetto <danielebenedetto@MacBook-Pro.station>
2024-09-04 14:04:21 +0200

    header responsive

--- src/utils/contents.ts
+++ src/utils/contents.ts
@@ -10,7 +10,79 @@ export const issues = [
     {
         title: 'Issue 3',
         date: '2021-10-03',
-    }
+    },
+    {
+        title: 'Issue 1',
+        date: '2021-10-01',
+    },
+    {
+        title: 'Issue 2',
+        date: '2021-10-02',
+    },
+    {
+        title: 'Issue 3',
+        date: '2021-10-03',
+    },
+    {
+        title: 'Issue 1',
+        date: '2021-10-01',
+    },
+    {
+        title: 'Issue 2',
+        date: '2021-10-02',
+    },
+    {
+        title: 'Issue 3',
+        date: '2021-10-03',
+    },
+    {
+        title: 'Issue 1',
+        date: '2021-10-01',
+    },
+    {
+        title: 'Issue 2',
+        date: '2021-10-02',
+    },
+    {
+        title: 'Issue 3',
+        date: '2021-10-03',
+    },
+    {
+        title: 'Issue 1',
+        date: '2021-10-01',
+    },
+    {
+        title: 'Issue 2',
+        date: '2021-10-02',
+    },
+    {
+        title: 'Issue 3',
+        date: '2021-10-03',
+    },
+    {
+        title: 'Issue 1',
+        date: '2021-10-01',
+    },
+    {
+        title: 'Issue 2',
+        date: '2021-10-02',
+    },
+    {
+        title: 'Issue 3',
+        date: '2021-10-03',
+    },
+    {
+        title: 'Issue 1',
+        date: '2021-10-01',
+    },
+    {
+        title: 'Issue 2',
+        date: '2021-10-02',
+    },
+    {
+        title: 'Issue 3',
+        date: '2021-10-03',
+    },
 ];
 
 export const navs = [
@@ -19,11 +91,29 @@ export const navs = [
     { name: "Films", href: "/contact" },
     { name: "Authors", href: "/services" },
     { name: "About", href: "/blog" },
-    { name: "Notes on Progress", href: "/notes", classes:"text-[16px] border-t border-black mt-4 pt-2" },
-    { name: "Books in Progress", href: "/books", classes:"text-[16px] !h-8 pb-2"}
+    { name: "Notes on Progress", href: "/notes", classes:"text-[14px] md:text-[16px] border-t border-black mt-4 pt-2" },
+    { name: "Books in Progress", href: "/books", classes:"text-[14px] md:text-[16px] !h-8 pb-2"}
 ];
 
 export const navFooter = {
     title: "Meet Viktor",
     content: "Although Viktor Zhdanov's name is little known today, he spearheaded one of the greatest projects in history. Who was he and what did he do?"
 }
+
+export const links = [
+    {
+        href: "/archive",
+        text: "Archive",
+        classes: "hidden"
+    },
+    {
+        href: "/notes",
+        text: "Notes",
+        classes: "hidden"
+    },
+    {
+        href: "/subscribe",
+        text: "Subscribe",
+        classes: "flex bg-black text-white border-r"
+    }
+];
